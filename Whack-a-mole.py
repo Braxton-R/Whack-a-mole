8b5203605b6ed61cdfe083db6ac490082e6e5f2a
Braxton-R <braxtonrope@rhs.school.nz>
2024-08-26 14:37:10 +1200

    Added a get_score function and a move_mole function
    Added a get_score function and a move_mole function and fixed some hard codded values

--- Whack-a-mole.py
+++ Whack-a-mole.py
@@ -3,20 +3,27 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import random
 
 class WhackAMole(QWidget):
     def __init__(self):
         super().__init__()
         self.NUM_OF_ROWS = 3
         self.NUM_OF_COLS = 3
+        self.MIN_NUM_OF_ROWS = 0
+        self.MIN_NUM_OF_COLS = 0
+        # -1 Because the computer starts counting from 0, not 1
+        self.MAX_NUM_OF_ROWS = self.NUM_OF_ROWS - 1
+        self.MAX_NUM_OF_COLS = self.NUM_OF_COLS - 1
         self.score = 0
+        self.current_mole_button = None
         # Starts timer
         self.start_timer()
         self.init_ui()
         
     # Set up the main layout
     def init_ui(self):
-        
+                
         self.setWindowTitle('Whack-A-Mole')
         self.setGeometry(100, 100, 600, 600)
 
@@ -29,6 +36,8 @@ class WhackAMole(QWidget):
 
         # Creates a grid of buttons using create_buttons function
         self.create_buttons()
+        
+        self.move_mole()
 
         self.setLayout(self.layout)
         self.show()
@@ -41,13 +50,13 @@ class WhackAMole(QWidget):
             for col in range(self.NUM_OF_COLS):
                 button = self.buttons[row][col]
                 button.setFixedSize(150,150)
-                button.clicked.connect(lambda ch, row=row, col=col: self.button_clicked(row, col))
+                button.clicked.connect(lambda ch, row=row, col=col: self.button_clicked())
                 self.layout.addWidget(button, row, col)
 
-    def button_clicked(self, row, col):
-        print(f'Button at ({row}, {col}) clicked')
+    def button_clicked(self):
         button = self.sender()
         if button.text() == 'MOLE':
+            button.setText('')
             self.score += 1
             self.move_mole()
 
@@ -61,18 +70,21 @@ class WhackAMole(QWidget):
         else:
             sys.exit(app.exec_())
 
-    def mole_clicked(self):
-        pass
-
     def move_mole(self):
-        pass
-
+        # Randomly selects a button and turns text to 'MOLE'
+        row = random.randint(self.MIN_NUM_OF_ROWS,self.MAX_NUM_OF_ROWS)
+        col = random.randint(self.MIN_NUM_OF_COLS,self.MAX_NUM_OF_COLS)
+        self.buttons[row][col].setText('MOLE')
+            
     def end_game(self):
         QMessageBox.information(self, 'Game Over!', 'Game over!')
+        self.get_score()
         sys.exit(app.exec_())
 
+    # Stores the score variable in a seperate txt file
     def get_score(self):
-        pass            
+        with open('score.txt', 'w') as file:
+            file.write(f'You got a score of: {self.score}')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
